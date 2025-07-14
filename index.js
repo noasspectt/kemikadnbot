1fd94eb64051e12e04967a4aea2c157eea1c8204
noasspectt <noasspect@gmail.com>
2025-07-14 04:06:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildVoiceStates
   ],
 });
 
